9020d325828fbb0c64a4697bee80a814fe5c8941
Giovanni Roin <giovanni.roin@gmail.com>
2025-08-31 19:16:50 +0200

    some linux scripts

---
+++ cmd/memory.sh
@@ -0,0 +1,8 @@
+set base="src/pax/base/export.c"
+set memory="src/pax/memory/export.c"
+
+set pax="$base $memory"
+set dir="src/pax/memory/test"
+
+zig cc --std=c99 $src "$dir/order.c"  -o memory_order.exe
+zig cc --std=c99 $src "$dir/system.c" -o memory_system.exe
---
+++ cmd/network.sh
@@ -0,0 +1,21 @@
+base="src/pax/base/export.c"
+memory="src/pax/memory/export.c"
+string="src/pax/string/export.c"
+format="src/pax/format/export.c"
+stream="src/pax/stream/export.c"
+network="src/pax/network/export.c"
+
+set pax="$base $memory $string $format $stream $network"
+
+set src="$pax"
+set dir="src/pax/network/test"
+
+gcc --std=c99 $src "$dir/address_ip4.c" -o network_address_ip4.exe
+gcc --std=c99 $src "$dir/address_ip6.c" -o network_address_ip6.exe
+gcc --std=c99 $src "$dir/address.c"     -o network_address.exe
+
+gcc --std=c99 $src "$dir/udp_server.c"  -o network_udp_server.exe
+gcc --std=c99 $src "$dir/udp_client.c"  -o network_udp_client.exe
+
+gcc --std=c99 $src "$dir/tcp_server.c"  -o network_tcp_server.exe
+gcc --std=c99 $src "$dir/tcp_client.c"  -o network_tcp_client.exe
